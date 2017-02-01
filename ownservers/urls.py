dbe3b12d1c3dbcba07eb2b5eefd9ae7d63f894d2
alexander <alexander.zayats@gmail.com>
2017-02-01 14:03:50 +0200

    Wipe everything for code review

--- ownservers/urls.py
+++
@@ -1,9 +0,0 @@
-from django.conf.urls import url
-from . import views
-
-app_name = 'ownservers'
-
-urlpatterns = [
-    url(r'^$', views.HomePage.as_view(), name='homepage'),
-    url(r'^(?P<person>\w+)/(?P<server>[-\w\+%_&]+)/$', views.ServersInformation.as_view(), name='server'),
-]
